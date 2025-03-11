f8699194592aa56848d31b925a20acbd90221224
CCEPCAHER <franklin_carranza@hotmail.com>
2025-03-11 08:52:49 +0100

    Actualizar script.js

--- script.js
+++ script.js
@@ -306,3 +306,8 @@ function resetApp() {
 
 // Inicia la aplicación al cargar el DOM
 window.addEventListener('DOMContentLoaded', initializeApp);
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(() => console.log("Service Worker registrado"))
+    .catch(err => console.log("Error en Service Worker:", err));
+}
