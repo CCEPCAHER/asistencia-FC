7d01577718fc8a719d48eefda002cb3d4019cf8a
CCEPCAHER <franklin_carranza@hotmail.com>
2025-03-11 08:51:52 +0100

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,23 @@
-// sw.js
-const CACHE_NAME = 'turnos-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  // Añade aquí otros recursos, como imágenes o íconos
-];
-
-self.addEventListener('install', (event) => {
-  // Precachear todos los recursos necesarios
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Caché abierta');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/manifest.json",
+        "/icons/icon-192x192.png",
+        "/icons/icon-512x512.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Retorna el recurso de la caché o haz fetch en red
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
